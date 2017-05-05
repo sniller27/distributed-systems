53512a9887baf26d4bc25833a268bb24f6607403
Jonas <sniller27@hotmail.com>
2017-05-06 01:18:19 +0200

    addArtists function added. more seperation between frontend and logic + code clean up

--- js/ArtistHandler.js
+++ js/ArtistHandler.js
@@ -1,36 +1,19 @@
-	var artists = [];
+//Artist array
+var artists = [];
 
+//Artist class
 var Artist = function(artistname, artistbplace, artistbday, artistfavorite){
-
-
-	
 	var favoritebool = artistfavorite ? true : false;
-	//generate artist ID
-	var genid = artists.length + 1;
+	this.id = artists.length + 1;
+	this.name = artistname;
+	this.birthPlace = artistbplace;
+	this.birthDate = artistbday;
+	this.favoriteArtist = favoritebool;
+}
 
-	//could also have been an instance of a class?
-	var artist = {
-		id: genid,
-		name:artistname, 
-		birthPlace:artistbplace, 
-		birthDate:artistbday, 
-		favoriteArtist:favoritebool
-	};
-	this.artistarray = artists;
-
-	/** METHODS **/
-	//add to array
-	this.addArtist = function(){
-		artists.push(artist);
-		console.log("pusher: " + artist);
-	};
-	//update table
-
-    console.log(JSON.stringify(artists));
-
-    this.getArtists = function() {
-    	return this.artistarray;
-	};
-
-	// this.getArtists = this.artistarray;
+//Add artist function
+function addArtist(artistname, artistbplace, artistbday, artistfavorite){
+	var artist = new Artist(artistname, artistbplace, artistbday, artistfavorite);
+	artists.push(artist);
+	console.log(artists);
 }
