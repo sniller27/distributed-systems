71baa4b4d9d8c6a3e62c135ace73b724e8f6372f
Jonas <sniller27@hotmail.com>
2017-05-06 21:23:53 +0200

    more OOP and encapsulated

--- js/ArtistHandler.js
+++ js/ArtistHandler.js
@@ -1,18 +1,33 @@
-//Artist array
-var artists = [];
-
 //Artist class
-var Artist = function(artistname, artistbplace, artistbday, artistfavorite){
+function Artist(artistid, artistname, artistbplace, artistbday, artistfavorite){
 	var favoritebool = artistfavorite ? true : false;
-	this.id = artists.length + 1;
-	this.name = artistname;
-	this.birthPlace = artistbplace;
-	this.birthDate = artistbday;
-	this.favoriteArtist = favoritebool;
-}
+	var id = artistid;
+	var name = artistname;
+	var birthPlace = artistbplace;
+	var birthDate = artistbday;
+	var favoriteArtist = favoritebool;
 
-//Add artist function
-function addArtist(artistname, artistbplace, artistbday, artistfavorite){
-	var artist = new Artist(artistname, artistbplace, artistbday, artistfavorite);
-	artists.push(artist);
+	this.getId = function(){
+		return id;
+	}
+
+	this.getName = function(){
+		return name;
+	}
+
+	this.getBirthPlace = function(){
+		return birthPlace;
+	}
+
+	this.getBirthDate = function(){
+		return birthDate;
+	}
+
+	this.getFavoriteArtist = function(){
+		return favoriteArtist;
+	}
+
+	this.setFavoriteArtist = function(favorite){
+		favoriteArtist = favorite;
+	}
 }
--- js/Main.js
+++ js/Main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 	/**
 		VARIABLES
 	**/
+	//Artist array
+	var artists = [];
+
 	//selector variables
 	var artistsearch = '#artistsearch';
 	var artisttabletbody = '#artisttable tbody';
@@ -10,6 +13,8 @@ $(document).ready(function(){
 	//form variables
 	var $form = $(addartistform);
 	var $successMsg = $(".alert");
+	//artist id's
+	var artistid = 0;
 
 	/** 
 		EVENTS
@@ -31,8 +36,9 @@ $(document).ready(function(){
 	//Updates artist list on table change
 	$(artisttabletbody).change(function(e){
 		var chagedartistid = $(e.target).get(0).id;
-		var currentbool = artists[chagedartistid-1].favoriteArtist;
-		artists[chagedartistid-1].favoriteArtist = !currentbool;
+		var currentbool = artists[chagedartistid-1].getFavoriteArtist();
+		console.log("current bool: " + currentbool);
+		artists[chagedartistid-1].setFavoriteArtist(!currentbool);
 	});
 
 	/** 
@@ -45,32 +51,44 @@ $(document).ready(function(){
     	var artistbday = $('#artistbirthdatefield').val();
     	var artistfavorite = $('#artistfavoritecheckbox').is(':checked');
 		
-		    addArtist(artistname, artistbplace, artistbday, artistfavorite);
-		    updateTable(artists, artisttabletbody)
-		    clearForm();
+	    addArtist(artistname, artistbplace, artistbday, artistfavorite);
+	    updateTable(artists, artisttabletbody)
+	    clearForm();
 
-		    //return false prevents site reload
-		    return false;
+	    //return false prevents site reload
+	    return false;
 	};
-	
+
+	//Add artist function
+	function addArtist(artistname, artistbplace, artistbday, artistfavorite){
+		artistid++;
+		var artist = new Artist(artistid, artistname, artistbplace, artistbday, artistfavorite);
+		console.log(artist.getName());
+		artists.push(artist);
+	}
+
 	//Update artist table
 	function updateTable(artistlist, tablerefresh){
 		var artistsearchword = $(artistsearch).val();
 	    var tabledata = "";
 
 		for (var i = 0; i < artistlist.length; i++) {
-			if (artistlist[i].name.includes(artistsearchword)) {
+			var getartistname = artistlist[i].getName();
+			console.log("artist name: " + getartistname);
+			if (getartistname.includes(artistsearchword)) {
 				tabledata += "<tr>";
 				for (var key in artistlist[i]) {
-				  if (artistlist[i].hasOwnProperty(key)) {
-				    if (key == 'favoriteArtist') {
+					//not completely dynamic solution
+				  if (artistlist[i].hasOwnProperty(key) && key != 'setFavoriteArtist') {
+				  	var artistfunction = eval("artistlist[i]['" + key + "']()");
+				    if (key == 'getFavoriteArtist') {
 				    	var ischecked = "";
-				    	if (artistlist[i][key] == true) {
+				    	if (artistlist[i].getFavoriteArtist() == true) {
 				    		ischecked = " checked";
 				    	}
-				    	tabledata += "<td><input type='checkbox' value='"+artistlist[i][key]+"' id='"+artistlist[i]['id']+"'"+ischecked+"></td>";
+				    	tabledata += "<td><input type='checkbox' value='"+artistfunction+"' id='"+artistlist[i].getId()+"'"+ischecked+"></td>";
 				    }else {
-				    	tabledata += "<td>"+artistlist[i][key]+"</td>";
+				    	tabledata += "<td>"+artistfunction+"</td>";
 				    }
 				  }
 				}
@@ -85,4 +103,5 @@ $(document).ready(function(){
 		$(addartistform).find('input:text, input[type=date]').val('');
     	$(addartistform).find('input[type=checkbox]').prop('checked', false);
 	}
+
 });
